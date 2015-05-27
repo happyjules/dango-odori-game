99dd199a5e651c363685f435371a1ecf972f2e85
chocofied <christievong@gmail.com>
2015-05-27 12:57:27 -0700

    cleaned up more

--- dango_files/dango.js
+++ dango_files/dango.js
@@ -2,68 +2,71 @@
 var canvas;
 var gl;
  
-// various parameters
+// various dango parameters
 var index = 0;
 var scaleFactor = 1;
 var h = 0; // height of dango
 var maxHeight = 5;
-var minHeight = 0; // not used in calculations yet. May be added to sin function.
+var minHeight = 0;
 var minx = 0.5;
 var miny = 0.5;
 var maxx = 1;
 var maxy = 1;
 var squishFactors = vec2(1,1);
 
-// this controls the pitch of the camera
-var theta = 0;
+// camera parameters
+var aspect;
+var near  = 0.1;
+var far   = 100;
+var fovy  = 45.0;
+
+var yaw   = 0;
+var theta = 0; // pitch
+
+var scoot = 0; // x-position
+var dist  = -5; // z-position
+var jump  = 0; // y-position
 var ypos = 0;
 
 // holds vertices and normals and texCoords
-var points = [];
+var points  = [];
 var normals = [];
 
 var positions = [
-    vec3(0, ypos, -5),
-    vec3(4, ypos, -3),
+    vec3( 0, ypos, -5),
+    vec3( 4, ypos, -3),
     vec3(-4, ypos, -4),
-    vec3(1, ypos, -4),
+    vec3( 1, ypos, -4),
     vec3(-2, ypos, -3)
 ];
 
+// DANGO
+// tetrahedron vertices
+var va = vec4( 0.0,      0.0,      -0.333333, 1);
+var vb = vec4( 0.0,      0.842809,  0.333333, 1);
+var vc = vec4(-0.86497, -0.271405,  0.333333, 1);
+var vd = vec4( 0.86497, -0.271405,  0.333333, 1);
+
 //color info for different colored dango
 var dangoColor = [
-    vec4( 0.588, 1, 0.71, 1.0 ), //green
-    vec4( 0.682, 0.47, 0.776, 1.0 ), //purple
-    vec4( 0.8, 0.462, 0.58, 1.0 ), //pink
-    vec4( 0.423, 0.654, 0.8, 1.0 ), //blue
-    vec4( 1, 1, 1, 1.0 ) //white
+    vec4( 0.588, 1,     0.71,  1.0 ), //green
+    vec4( 0.682, 0.47,  0.776, 1.0 ), //purple
+    vec4( 0.8,   0.462, 0.58,  1.0 ), //pink
+    vec4( 0.423, 0.654, 0.8,   1.0 ), //blue
+    vec4( 1,     1,     1,     1.0 ) //white
 ]
 
-// holds color info for dango in this order: material ambient color, material diffuse color, and material specular color
+// holds color info
 var colors = [
     vec4( 0.5, 0.5, 0.5, 1.0 ), // grey
     vec4( 0.9, 0.9, 0.9, 1.0 ), // almost white
     vec4( 1.0, 1.0, 1.0, 1.0 ), // white
-    vec4( 0.0, 0.0, 0.0, 1.0 ) // black 
+    vec4( 0.0, 0.0, 0.0, 1.0 ), // black
+    vec4( 1.0, 0.3, 0.3, 1.0 ) // red?
 ];
 
-// camera parameters
-var near  = 0.1;
-var far   = 100;
-var fovy  = 45.0;
-var yaw   = 0;
-var scoot = 0;
-var dist  = -5;
-var jump  = 0;
-var aspect;
 
-// tetrahedron vertices
-var va = vec4(0.0, 0.0, -.3,1);
-var vb = vec4(0.0, 0.842809, 0.333333, 1);
-var vc = vec4(-0.86497, -0.271405, 0.333333, 1);
-var vd = vec4(0.86497, -0.271405, 0.333333,1);
-
-
+// ROOM
 // cube vertices
 var roomVertices = [
     vec4( -0.5, -0.5,  0.5, 1.0 ),
@@ -88,31 +91,29 @@ function quad(a, b, c, d) {
 }
 
 function colorCube() {
-    quad( 1, 0, 3, 2 );
-    for(var i =0; i <6; i++)    
+    quad( 1, 0, 3, 2 ); // front
+    for(var i = 0; i < 6; i++)    
         normals.push(0,0,-1,0);
+
     quad( 2, 3, 7, 6 );
-
-    for(var i =0; i <6; i++)    
+    for(var i = 0; i < 6; i++)    
         normals.push(-1,0,0,0);
+
     quad( 3, 0, 4, 7 );
-
-    for(var i =0; i <6; i++)    
+    for(var i = 0; i < 6; i++)    
         normals.push(0,1,0,0);
-    quad( 6, 5, 1, 2 );
 
-    for(var i =0; i <6; i++)    
+    quad( 6, 5, 1, 2 );
+    for(var i = 0; i < 6; i++)    
         normals.push(0,-1,0,0);
     
     quad( 4, 5, 6, 7 );
-    
-    for(var i =0; i <6; i++)    
+    for(var i = 0; i < 6; i++)    
         normals.push(0,0,1,0);
-    quad( 5, 4, 0, 1 );
 
-    for(var i =0; i <6; i++)    
+    quad( 5, 4, 0, 1 );
+    for(var i = 0; i < 6; i++)    
         normals.push(1,0,0,0);
-    
 }
 
 // light information
@@ -136,32 +137,14 @@ var apLoc, dpLoc, spLoc, shininessLoc;
 
 // pushes vertices and normals of triangle
 function triangle(a, b, c, complexity) {
-     
-     points.push(a);
-     points.push(b);
-     points.push(c);
-     index += 3;
+    points.push(a);
+    points.push(b);
+    points.push(c);
+    index += 3;
     
-     /*if (flatShading) { // per triangle normals
-        var normal = computeFlat(a,b,c);
-        for (var i = 0; i < 3; i++)
-            normalsArray.push(normal[0], normal[1], normal[2], 0.0);
-        */
-     //} else { // analytical normals
-        normals.push(a[0],a[1], a[2], 0.0);
-        normals.push(b[0],b[1], b[2], 0.0);
-        normals.push(c[0],c[1], c[2], 0.0);
-    //}
-}
-
-// computes flat normals
-function computeFlat(a, b, c){
-    var u = subtract(b,a);
-    var v = subtract(c,a);
-
-    var normal = normalize(cross(v,u));
-
-    return normal;
+    normals.push(a[0],a[1], a[2], 0.0);
+    normals.push(b[0],b[1], b[2], 0.0);
+    normals.push(c[0],c[1], c[2], 0.0);
 }
 
 // recursively determines vertices of sphere
@@ -194,13 +177,13 @@ function tetrahedron(a, b, c, d, n) {
     divideTriangle(a, c, d, n, n);
 }
 
-
-
+// calculates height of dango wrt time
 function bounceHeight(t) {
     h = maxHeight*Math.pow((Math.sin(t/1000)), 2);
     return h;
 }
 
+// calculates scaling factors of dango wrt height
 function squish(t) {
     var j = maxHeight*Math.pow(Math.sin(2*t/1000), 2);
     var my = maxHeight/(maxx - minx); //-4
@@ -357,7 +340,7 @@ function render(t) {
 
         // EYES
         var eye1 = squishMatrix;
-        eye1 = mult(eye1, translate(-.1, 0, .5));
+        eye1 = mult(eye1, translate(-0.1, 0, .5));
         eye1 = mult(eye1, scale(0.04, 0.3, .6));
         gl.uniformMatrix4fv(squishMatrixLoc, false, flatten(eye1));
 
@@ -375,7 +358,7 @@ function render(t) {
         gl.drawArrays( gl.TRIANGLES, 0, index);
 
         var eye2 = squishMatrix;
-        eye2 = mult(eye2, translate(.1, 0, 0.5));
+        eye2 = mult(eye2, translate(0.1, 0, 0.5));
         eye2 = mult(eye2, scale(0.04, 0.3, .6));
         gl.uniformMatrix4fv(squishMatrixLoc, false, flatten(eye2));
         // draw eye2
@@ -385,22 +368,37 @@ function render(t) {
     }
 
     // ROOM
-    squishMatrix = mat4();
+    squishMatrix = scale(10, 10, 10);
     gl.uniformMatrix4fv(squishMatrixLoc,false,flatten(squishMatrix));
     // build model view matrix
-    modelViewMatrix = mult(translate(0,4.5,0), scale(10, 10, 10));
+    modelViewMatrix = translate(0, 4.5, 0);
     // send model view matrix to html file
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     // set colors
     ambientProduct  = mult(lightArray[0], colors[0]);
     diffuseProduct  = mult(lightArray[1], colors[2]);
-    //if (don't want specular product)
-    specularProduct = vec4(0,0,0,0);
+    specularProduct = vec4(0, 0, 0, 0);
     // send colors to shader
     gl.uniform4fv( apLoc, flatten(ambientProduct) );
     gl.uniform4fv( dpLoc, flatten(diffuseProduct) );
     gl.uniform4fv( spLoc, flatten(specularProduct) );
     // draw room
+    gl.drawArrays(gl.TRIANGLES, index, 36);
+
+    // CHOPSTICKS
+    squishMatrix = scale(1, 1, 10);
+    gl.uniformMatrix4fv(squishMatrixLoc,false,flatten(squishMatrix));
+    modelViewMatrix = mult(translate(0, 4.5, 0), rotate(30, [0,1,0]));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    // set colors
+    ambientProduct  = mult(lightArray[0], colors[3]);
+    diffuseProduct  = mult(lightArray[1], colors[2]);
+    specularProduct = vec4(0, 0, 0, 0);
+    // send colors to shader
+    gl.uniform4fv( apLoc, flatten(ambientProduct) );
+    gl.uniform4fv( dpLoc, flatten(diffuseProduct) );
+    gl.uniform4fv( spLoc, flatten(specularProduct) );
+    // draw chopsticks
     gl.drawArrays(gl.TRIANGLES, index, 36);
 
     window.requestAnimFrame(render);
